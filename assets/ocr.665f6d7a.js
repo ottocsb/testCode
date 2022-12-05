57916621b0ee695e394a95a03213462a9fe31d28
ottocsb <ottocsb@users.noreply.github.com>
2022-12-05 15:55:54 +0000

    deploy: 012cd04a9921177d1f9f6caceb2c218089aaf194

--- assets/ocr.665f6d7a.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./index.03f38caf.js";const i={__name:"ocr",setup(r){const t=e.createWorker({logger:a=>console.log(a)});return(async()=>{await t.load(),await t.loadLanguage("chi_sim"),await t.initialize("chi_sim");let{data:{text:a}}=await t.recognize("/src/assets/test1.jpg");await t.terminate(),console.log(a)})(),(a,o)=>null}};export{i as default};
